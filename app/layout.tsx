be83b158d6415015fa5a1ddcc532802eaadfd1f4
z0st <alejandre.bonnin@gmail.com>
2023-11-23 11:32:24 +0100

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import './globals.css'
 const font = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Genius',
-  description: 'AI Platform',
+  title: 'Exlabel',
+  description: 'Digital labels',
 }
 
 export default async function RootLayout({
